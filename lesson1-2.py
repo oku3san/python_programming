e9684eec02a57e58c63ed45439c3bd689744e8af
oku3san <takaya0303@gmail.com>
2023-11-13 02:01:52 +0900

    Update

---
+++ lesson1-2.py
@@ -0,0 +1,24 @@
+print('Hello, \nHow are you?')
+
+print("=====")
+print("""
+aaa
+bbb
+ccc
+""")
+print("=====")
+
+print("=====")
+print("""\
+aaa
+bbb
+ccc\
+""")
+print("=====")
+
+prefix = 'Py'
+print(prefix + 'thon')
+
+s = ('aaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbb')
+print(s)

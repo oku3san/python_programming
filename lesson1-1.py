52dc090f4b346ce61561c5f31829c912b261c7e2
oku3san <takaya0303@gmail.com>
2023-11-13 01:56:13 +0900

    Update

---
+++ lesson1-1.py
@@ -0,0 +1,31 @@
+num = 1
+print(num)
+
+name = 'Mike'
+print(name)
+
+print(num, type(num))
+print(name, type(name))
+
+is_ok = True
+print(is_ok, type(is_ok))
+
+name2 = '1'
+new_num = int(name2)
+print(new_num, type(new_num))
+
+print('Hi', 'Mike', sep='m')
+
+print(5 ** 5)
+
+a = 1.23456
+print(round(a, 3))
+
+import math
+
+result = math.sqrt(25)
+print(result)
+
+resultLog = math.log2(10)
+print(resultLog)
+print(help(math))

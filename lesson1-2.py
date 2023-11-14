18df2069b6ddbdf9bffcfceca8d848a4954d6573
oku3san <takaya0303@gmail.com>
2023-11-14 23:16:51 +0900

    Update

--- lesson1-2.py
+++ lesson1-2.py
@@ -27,6 +27,9 @@ word = "python"
 print(word[0], word[-1])
 print(word[:4], word[4:])
 
-s = 'My name is Mile. Hi Mike.'
+s = 'My name is Mike. Hi Mike.'
 is_start = s.startswith('My')
 print(is_start)
+
+print(s.find('Mike'))
+print(s.rfind('Mike'))

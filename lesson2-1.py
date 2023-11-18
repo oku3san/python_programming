4c4fc581ed74d5cf8d759b4af8a374be78202c0f
oku3san <takaya0303@gmail.com>
2023-11-18 23:22:23 +0900

    Update

---
+++ lesson2-1.py
@@ -0,0 +1,20 @@
+l = [1, 20, 30, 50]
+print(l)
+print(l[0])
+print(type(l))
+print(list('abcdefg'))
+
+n = [1, 10, 100, 1000, 10000]
+print(n)
+print(n.pop())
+print(n)
+
+###
+
+x = [1, 2, 3]
+y = [7, 8, 9]
+x.extend(y)
+print(x)
+print(y)
+print(x.index(3))
+print(x.count(3))

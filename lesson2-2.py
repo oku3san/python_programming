656fccc5191ea567138841a53f9b0ea745af0d4c
oku3san <takaya0303@gmail.com>
2023-11-19 01:34:21 +0900

    Update

--- lesson2-2.py
+++ lesson2-2.py
@@ -1,18 +1,26 @@
-t = (1, 2, 3, 4, 5, 1, 4)
-print(t)
-print(type(t))
-# t[0] = 100
-print(t[0])
+# t = (1, 2, 3, 4, 5, 1, 4)
+# print(t)
+# print(type(t))
+# # t[0] = 100
+# print(t[0])
+#
+# new_tuple = (1, 2, 3) + (4, 5, 6)
+# print(new_tuple)
+#
+# x, y = 10, 20
+# print(type(x))
+# print(x, y)
+#
+# a = 100
+# b = 200
+# print(a, b)
+# a, b = b, a
+# print(a, b)
 
-new_tuple = (1, 2, 3) + (4, 5, 6)
-print(new_tuple)
+choose_from_three = ("A", "B", "C")
+answer = []
+answer.append('A')
+answer.append('C')
 
-x, y = 10, 20
-print(type(x))
-print(x, y)
-
-a = 100
-b = 200
-print(a, b)
-a, b = b, a
-print(a, b)
+print(choose_from_three)
+print(answer)

d79209ebfd6214aa0b1c67960ce4732326d4f822
oku3san <takaya0303@gmail.com>
2023-11-19 00:23:06 +0900

    Update

--- lesson2-1.py
+++ lesson2-1.py
@@ -1,32 +1,66 @@
-l = [1, 20, 30, 50]
-print(l)
-print(l[0])
-print(type(l))
-print(list('abcdefg'))
-
-n = [1, 10, 100, 1000, 10000]
-print(n)
-print(n.pop())
-print(n)
-
-###
-
-x = [1, 2, 3]
-y = [7, 8, 9]
-x.extend(y)
-print(x)
-print(y)
-print(x.index(3))
-print(x.count(3))
-
-###
-
-r = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 10, 7]
-if 5 in r:
-    print('exists')
-r.sort()
-print(r)
-r.sort(reverse=True)
-print(r)
-
-###
+# l = [1, 20, 30, 50]
+# print(l)
+# print(l[0])
+# print(type(l))
+# print(list('abcdefg'))
+#
+# n = [1, 10, 100, 1000, 10000]
+# print(n)
+# print(n.pop())
+# print(n)
+#
+# ###
+#
+# x = [1, 2, 3]
+# y = [7, 8, 9]
+# x.extend(y)
+# print(x)
+# print(y)
+# print(x.index(3))
+# print(x.count(3))
+#
+# ###
+#
+# r = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 10, 7]
+# if 5 in r:
+#     print('exists')
+# r.sort()
+# print(r)
+# r.sort(reverse=True)
+# print(r)
+#
+# ###
+#
+# i = [1, 2, 3, 4, 5]
+# j = i
+# j[0] = 100
+# print('j=', j)
+# print('i=', i)
+#
+# ###
+#
+# x = [1, 2, 3, 4, 5]
+# y = x.copy()
+# y[0] = 100
+# print('x=', x)
+# print('y=', y)
+#
+# ###
+#
+# X = 20
+# Y = X
+# Y = 5
+# print(id(X))
+# print(id(Y))
+# print(Y)
+# print(X)
+#
+# ###
+#
+# X = ['a', 'b']
+# Y = X
+# Y[0] = 'p'
+# print(id(X))
+# print(id(Y))
+# print(Y)
+# print(X)

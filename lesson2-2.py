887e6d0e326efc0c80ab9cce17b24d91bb338b4c
oku3san <takaya0303@gmail.com>
2023-11-19 01:27:05 +0900

    Update

---
+++ lesson2-2.py
@@ -0,0 +1,18 @@
+t = (1, 2, 3, 4, 5, 1, 4)
+print(t)
+print(type(t))
+# t[0] = 100
+print(t[0])
+
+new_tuple = (1, 2, 3) + (4, 5, 6)
+print(new_tuple)
+
+x, y = 10, 20
+print(type(x))
+print(x, y)
+
+a = 100
+b = 200
+print(a, b)
+a, b = b, a
+print(a, b)

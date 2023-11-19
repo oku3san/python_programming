32fd8720047a7542ba45bd30f39dfc5c9c6fc282
oku3san <takaya0303@gmail.com>
2023-11-20 00:13:06 +0900

    Update

--- lesson3-3.py
+++ lesson3-3.py
@@ -1,4 +1,29 @@
-count = 0
-while count < 5:
-    print(count)
-    count += 1
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+
+# count = 0
+# while True:
+#     if count >= 5:
+#         break
+#     print(count)
+#     count += 1
+
+# count = 0
+# while True:
+#     if count >= 5:
+#         break
+#     if count == 2:
+#         count += 1
+#         continue
+#     print(count)
+#     count += 1
+
+# count = 0
+#
+# while count <= 5:
+#     print(count)
+#     count += 1
+# else:
+#     print('Done')

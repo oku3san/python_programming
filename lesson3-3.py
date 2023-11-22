30d85b6b23e0b1e8b65b458ba9dff0992170bf10
oku3san <takaya0303@gmail.com>
2023-11-22 21:00:00 +0900

    Update

--- lesson3-3.py
+++ lesson3-3.py
@@ -27,3 +27,50 @@
 #     count += 1
 # else:
 #     print('Done')
+
+# while True:
+#     word = input('Enter:')
+#     if word == 'ok':
+#         break
+#     print('next')
+
+# while True:
+#     word = input('Enter:')
+#     print(type(word))
+#     num = int(word)
+#     print(type(num))
+#     if num == 100:
+#         break
+#     print('next')
+
+# some_list = [1, 2, 3, 4, 5]
+# i = 0
+# print(len(some_list))
+# while i < len(some_list):
+#     print(some_list[i])
+#     i += 1
+
+# some_list = [1, 2, 3, 4, 5, 9]
+# for i in some_list:
+#     print(i)
+
+# for _ in range(10):
+#     print('hello')
+
+# i = 0
+# for i, fruit in enumerate(['apple', 'banana', 'orange']):
+#     print(i, fruit)
+
+# days = ['Mon', 'Tue', 'Wed']
+# fruits = ['apple', 'banana', 'orange']
+# drinks = ['coffee', 'tea', 'beer']
+#
+# for day, fruit, drink in zip(days, fruits, drinks):
+#     print((day, fruit, drink))
+
+d = {
+    'x': 100,
+    'y': 200
+}
+for k, v in d.items():
+    print(k, ':', v)

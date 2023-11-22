2f770b286efa99b671310dcac904dbbc91b35369
oku3san <takaya0303@gmail.com>
2023-11-23 01:20:41 +0900

    Update

--- lesson4-1.py
+++ lesson4-1.py
@@ -1,13 +1,19 @@
-def say_something():
-    s = 'hi'
-    return s
-
-
-# say_something()
-# print(type(say_something()))
+# def say_something():
+#     s = 'hi'
+#     return s
 #
-# f = say_something
-# f()
+#
+# # say_something()
+# # print(type(say_something()))
+# #
+# # f = say_something
+# # f()
+#
+# result = say_something()
+# print(result)
 
-result = say_something()
-print(result)
+def what_is_this(color):
+    print(color)
+
+
+what_is_this('red')

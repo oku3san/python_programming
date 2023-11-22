abed078a7e0c535683d0ef4d1fdf5874ca9d5589
oku3san <takaya0303@gmail.com>
2023-11-23 01:23:12 +0900

    Update

--- lesson4-1.py
+++ lesson4-1.py
@@ -12,8 +12,20 @@
 # result = say_something()
 # print(result)
 
+# def what_is_this(color):
+#     print(color)
+#
+#
+# what_is_this('red')
+
 def what_is_this(color):
-    print(color)
+    if color == 'red':
+        return 'tomato'
+    elif color == 'green':
+        return 'green pepper'
+    else:
+        return "I don't know"
 
 
-what_is_this('red')
+result = what_is_this('a')
+print(result)

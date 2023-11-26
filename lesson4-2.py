9cad78f1aa96187aa76bc89dcc48bd8ef747b238
oku3san <takaya0303@gmail.com>
2023-11-26 23:34:55 +0900

    Update

--- lesson4-2.py
+++ lesson4-2.py
@@ -8,13 +8,13 @@
 #
 # outer(1, 2)
 
-def circle_area_func(pi):
-    def circle_area(radius):
-        return pi * radius * radius
-
-    return circle_area
-
-
-ca1 = circle_area_func(3.14)
-print(ca1)
-print(ca1(10))
+# def circle_area_func(pi):
+#     def circle_area(radius):
+#         return pi * radius * radius
+#
+#     return circle_area
+#
+#
+# ca1 = circle_area_func(3.14)
+# print(ca1)
+# print(ca1(10))

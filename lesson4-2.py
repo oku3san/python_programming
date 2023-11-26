3984f6cd11b932a02dd282279738f7adce5c8606
oku3san <takaya0303@gmail.com>
2023-11-26 23:55:13 +0900

    Update

--- lesson4-2.py
+++ lesson4-2.py
@@ -47,20 +47,20 @@
 # r = f(10, 20)
 # print(r)
 
-def print_info(func):
-    def wrapper(*args, **kwargs):
-        print('start')
-        result = func(*args, **kwargs)
-        print('end')
-        return result
-
-    return wrapper
-
-
-@print_info
-def add_num(a, b):
-    return a + b
-
-
-r = add_num(10, 20)
-print(r)
+# def print_info(func):
+#     def wrapper(*args, **kwargs):
+#         print('start')
+#         result = func(*args, **kwargs)
+#         print('end')
+#         return result
+#
+#     return wrapper
+#
+#
+# @print_info
+# def add_num(a, b):
+#     return a + b
+#
+#
+# r = add_num(10, 20)
+# print(r)

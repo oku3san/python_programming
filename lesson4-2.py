3a9f6b651d240c113063ee599688f8d482e61cca
oku3san <takaya0303@gmail.com>
2023-11-26 23:46:44 +0900

    Update

--- lesson4-2.py
+++ lesson4-2.py
@@ -28,3 +28,39 @@
 # print('end')
 #
 # print(r)
+
+# def print_info(func):
+#     def wrapper(*args, **kwargs):
+#         print('start')
+#         result = func(*args, **kwargs)
+#         print('end')
+#         return result
+#
+#     return wrapper
+#
+#
+# def add_num(a, b):
+#     return a + b
+#
+#
+# f = print_info(add_num)
+# r = f(10, 20)
+# print(r)
+
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+
+    return wrapper
+
+
+@print_info
+def add_num(a, b):
+    return a + b
+
+
+r = add_num(10, 20)
+print(r)

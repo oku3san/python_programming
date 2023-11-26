0e0928bad97be98bf0af402fa26ae518dc9af940
oku3san <takaya0303@gmail.com>
2023-11-26 19:48:19 +0900

    Update

--- lesson4-1.py
+++ lesson4-1.py
@@ -85,3 +85,16 @@
 #
 #
 # menu(entree='beef', drink='coffee')
+
+def menu(**kwargs):
+    for k, v in kwargs.items():
+        print(k, v)
+
+
+d = {
+    'entree': 'beef',
+    'drink': 'ice coffee',
+    'dessert': 'ice',
+}
+
+menu(**d)

ad6a95e73c20171b09eddb6a326c08e2083d63fe
oku3san <takaya0303@gmail.com>
2023-11-26 23:34:21 +0900

    Update

---
+++ lesson4-2.py
@@ -0,0 +1,20 @@
+# def outer(a, b):
+#     def plus(c, d):
+#         return c + d
+#
+#     r = plus(a, b)
+#     print(r)
+#
+#
+# outer(1, 2)
+
+def circle_area_func(pi):
+    def circle_area(radius):
+        return pi * radius * radius
+
+    return circle_area
+
+
+ca1 = circle_area_func(3.14)
+print(ca1)
+print(ca1(10))

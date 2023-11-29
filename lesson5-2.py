cd47a4903391bf53fa37f69302f114745fe024cf
oku3san <takaya0303@gmail.com>
2023-11-30 00:20:44 +0900

    Update

--- lesson5-2.py
+++ lesson5-2.py
@@ -11,3 +11,17 @@ print(sorted(ranking))
 print(sorted(ranking, key=ranking.get))
 print(ranking.get('A'))
 print(sorted(ranking, key=ranking.get, reverse=True))
+
+s = 'aldjflkadjfoiefbadaogia'
+# d = {}
+# for c in s:
+#     d.setdefault(c, 0)
+#     d[c] += 1
+# print(d)
+
+from collections import defaultdict
+
+d = defaultdict(int)
+for c in s:
+    d[c] += 1
+print(d)

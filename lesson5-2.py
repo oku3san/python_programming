00430543f875e47e09747acc37786d50e62f5a01
oku3san <takaya0303@gmail.com>
2023-11-30 00:24:55 +0900

    Update

--- lesson5-2.py
+++ lesson5-2.py
@@ -1,27 +1,39 @@
-ranking = {
-    'A': 100,
-    'B': 85,
-    'C': 92
-}
+# # ranking = {
+# #     'A': 100,
+# #     'B': 85,
+# #     'C': 92
+# # }
+# #
+# # for key in ranking:
+# #     print(key)
+# #
+# # print(sorted(ranking))
+# # print(sorted(ranking, key=ranking.get))
+# # print(ranking.get('A'))
+# # print(sorted(ranking, key=ranking.get, reverse=True))
+# #
+# # s = 'aldjflkadjfoiefbadaogia'
+# # # d = {}
+# # # for c in s:
+# # #     d.setdefault(c, 0)
+# # #     d[c] += 1
+# # # print(d)
+# #
+# # from collections import defaultdict
+# #
+# # d = defaultdict(int)
+# # for c in s:
+# #     d[c] += 1
+# # print(d)
+#
+#
+# import config
+#
+# print('config:', __name__)
 
-for key in ranking:
-    print(key)
+def main():
+    print('main')
 
-print(sorted(ranking))
-print(sorted(ranking, key=ranking.get))
-print(ranking.get('A'))
-print(sorted(ranking, key=ranking.get, reverse=True))
 
-s = 'aldjflkadjfoiefbadaogia'
-# d = {}
-# for c in s:
-#     d.setdefault(c, 0)
-#     d[c] += 1
-# print(d)
-
-from collections import defaultdict
-
-d = defaultdict(int)
-for c in s:
-    d[c] += 1
-print(d)
+if __name__ == '__main__':
+    main()

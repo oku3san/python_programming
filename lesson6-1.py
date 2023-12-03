2d63aa9ba264d079e063bb6685b38203b8488192
oku3san <takaya0303@gmail.com>
2023-12-04 00:22:24 +0900

    Update

--- lesson6-1.py
+++ lesson6-1.py
@@ -14,3 +14,14 @@
 #
 # person = Person()
 # person.say_something()
+
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+
+    def say_something(self):
+        print('I am {}. hello'.format(self.name))
+
+
+person = Person('Mike')
+person.say_something()

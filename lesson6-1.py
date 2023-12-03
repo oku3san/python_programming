0f281765f80aa0f1cff9a3b14648e3162aa51e6b
oku3san <takaya0303@gmail.com>
2023-12-04 00:20:45 +0900

    Update

--- lesson6-1.py
+++ lesson6-1.py
@@ -1,16 +1,16 @@
-s = 'alkdjflajlabjie'
-print(s.capitalize())
-
-
-class Person(object):
-    def __init__(self, name):
-        self.name = name
-        print('First')
-        print(self.name)
-
-    def say_something(self):
-        print('hello')
-
-
-person = Person('Mike')
-person.say_something()
+# s = 'alkdjflajlabjie'
+# print(s.capitalize())
+#
+#
+# class Person(object):
+#     def __init__(self, name='Default'):
+#         self.name = name
+#         print('First')
+#         print(self.name)
+#
+#     def say_something(self):
+#         print('hello')
+#
+#
+# person = Person()
+# person.say_something()
